3d053f866f7d348c96e38587535150698483e865
kovalkl <anastasiiakovalklkl@gmail.com>
2025-02-13 23:05:55 +0300

    feat: implement labeled input component

--- frontend/src/components/FormStepper/components/LabeledInput/LabeledInput.tsx
+++ frontend/src/components/FormStepper/components/LabeledInput/LabeledInput.tsx
@@ -1,27 +1,50 @@
+import { Control, Controller, FieldErrors } from 'react-hook-form';
+
+import { TITLES } from '@/components/FormStepper/components/AdBasicInfoForm/constants';
+import { AdBasicInfoFormValues } from '@/components/FormStepper/components/AdBasicInfoForm/schema';
 import TextField, { TextFieldProps } from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+type TitleKey = keyof typeof TITLES;
+
 type LabeledInputProps = {
-  label: string;
+  type: TitleKey;
   isRequired: boolean;
+  control: Control<AdBasicInfoFormValues>;
+  errors: FieldErrors<AdBasicInfoFormValues>;
 } & TextFieldProps;
 
 export const LabeledInput = ({
-  label,
+  type,
   isRequired,
+  control,
+  errors,
   ...props
 }: LabeledInputProps) => {
   return (
-    <>
-      <Typography variant='body1'>
-        {`${label} `}
-        {isRequired && (
-          <Typography component='span' variant='caption'>
-            *
+    <Controller
+      name={type}
+      control={control}
+      render={({ field }) => (
+        <>
+          <Typography variant='body1'>
+            {`${TITLES[type]} `}
+            {isRequired && (
+              <Typography component='span' variant='caption'>
+                *
+              </Typography>
+            )}
           </Typography>
-        )}
-      </Typography>
-      <TextField required helperText=' ' {...props} />
-    </>
+          <TextField
+            {...field}
+            value={field.value || ''}
+            required
+            helperText={errors[type]?.message || ' '}
+            error={!!errors[type]}
+            {...props}
+          />
+        </>
+      )}
+    />
   );
 };
